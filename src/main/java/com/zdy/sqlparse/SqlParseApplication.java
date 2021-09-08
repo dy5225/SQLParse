441ba985685ea274e01489c795a3871b80670cc2
zdy <dy_5225@163.com>
2021-09-08 16:56:18 +0800

    new project

---
+++ src/main/java/com/zdy/sqlparse/SqlParseApplication.java
@@ -0,0 +1,13 @@
+package com.zdy.sqlparse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlParseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlParseApplication.class, args);
+    }
+
+}
